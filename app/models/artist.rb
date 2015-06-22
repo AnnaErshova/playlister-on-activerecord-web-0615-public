c66a38e36e4acbb436fa101fedd6fc93d607f53c
AnnaErshova <anna.ershova@gmail.com>
2015-06-22 12:47:04 -0400

    all done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ActiveRecord::Base
+  has_many :genres, through: :songs
+  has_many :songs
 end
+
+# http://guides.rubyonrails.org/association_basics.html
--- db/migrations/01_create_songs.rb
+++ db/migrations/01_create_songs.rb
@@ -1,2 +1,11 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.text :name
+      t.integer :artist_id # equivalent of foreign keys in SQL
+      t.integer :genre_id
+      t.timestamps null: false
+    end
+  end
 end
+
